3cc27982b47abc0089cc862bdbe721aa6472aa82
Yaser Soheilifar <83553075+ysoheilifar@users.noreply.github.com>
2023-01-02 18:17:34 +0330

    Update vpnchainroute.sh

--- vpnchainroute.sh
+++ vpnchainroute.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+green='\033[0;32m'
+red='\033[0;31m'
+yellow='\033[0;33m'
+nc='\033[0m' #No Color
 read -p "Enter Server-1 IP address: " server_1
 gw=`ip route | grep default | cut -d" " -f3`
 ether=`ip route | grep default | cut -d" " -f5`
@@ -10,7 +15,7 @@ if [[ -z $iptables ]]; then
 fi
 stunnel
 openvpn --config /root/irfree.ovpn --daemon
-echo -e "\n wait 30 sec ... \n"
+echo -e "\n${yellow} wait 30 sec ... ${nc}\n"
 sleep 30
 iprule=`ip rule show table 120`
 if [[ -z $iprule  ]]; then
