50a9d679c27c6ea711da7244d75a962b65b74b81
Yaser Soheilifar <83553075+ysoheilifar@users.noreply.github.com>
2023-01-02 18:16:35 +0330

    Update vpnchainroute.sh

--- vpnchainroute.sh
+++ vpnchainroute.sh
@@ -1,6 +1,7 @@
+read -p "Enter Server-1 IP address: " server_1
 gw=`ip route | grep default | cut -d" " -f3`
 ether=`ip route | grep default | cut -d" " -f5`
-ip route add 95.217.159.120 via $gw dev $ether
+ip route add $server_1 via $gw dev $ether
 iptables=`iptables -t nat -L | grep -e 192.168.0.0 -e 10.8.0.2`
 if [[ -z $iptables ]]; then
     iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
